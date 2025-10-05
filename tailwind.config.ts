2b07bf2fcfbd9d88f530a85d93c943c6ccfe35de
jcaisystems <jcaisystems@gmail.com>
2025-10-05 14:55:56 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
+  safelist: [
+    "animate-wave-glow"
+  ],
   theme: {
     container: {
       center: true,
